dfdd63e103a4c985b22f8b6828a263ed7edee73f
taylorvercillo <taylornvcasey@gmail.com>
2021-11-08 22:14:21 -0700

    All Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const record = [
+    {year: "2018", result: "L"}, 
+    {year: "2017", result: "W"}, 
+    {year: "2016", result: "N/A"}
+]
 
+function superbowlWin(score) {
+    for (const record of score) {
+        if (record.result === "W") {
+            return record.year; 
+        }
+    }
+}
